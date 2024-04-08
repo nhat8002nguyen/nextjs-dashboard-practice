c7aa9d3ce8e24466369fd8fd2b012df66f009579
nhat8002nguyen <nv.nhat8002@gmail.com>
2024-04-08 17:10:25 +0700

    completed the next.js official beginer course

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchCardData } from '../../lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Dashboard"
+}
  
 export default async function Page() {
   return (
